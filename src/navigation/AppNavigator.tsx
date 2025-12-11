3ae2917e12577641777a2e6b3dc859e38e9f1ccb
GESTER.SAMSON <samsongester@gmail.com>
2025-12-11 15:03:27 +0800

    Add basic geolocation hunt mode screen

--- src/navigation/AppNavigator.tsx
+++ src/navigation/AppNavigator.tsx
@@ -7,6 +7,7 @@ import SigninScreen from '../screens/auth/SigninScreen';
 import SignupScreen from '../screens/auth/SignupScreen';
 import StarterScreen from '../screens/start/StarterScreen';
 import GenderScreen from '../screens/start/GenderScreen';
+import HuntScreen from '../screens/HuntScreen'; // ⬅️ NEW
 
 const Stack = createNativeStackNavigator();
 
@@ -14,12 +15,42 @@ export default function AppNavigator() {
   return (
     <NavigationContainer>
       <Stack.Navigator>
-        <Stack.Screen name="Introduction" component={IntroductionScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Signin" component={SigninScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Signup" component={SignupScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Starter" component={StarterScreen} options={{ headerShown: false }} />
-        <Stack.Screen name="Gender" component={GenderScreen} options={{ headerShown: false }} />
+        <Stack.Screen
+          name="Introduction"
+          component={IntroductionScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Signin"
+          component={SigninScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Signup"
+          component={SignupScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Starter"
+          component={StarterScreen}
+          options={{ headerShown: false }}
+        />
+        <Stack.Screen
+          name="Gender"
+          component={GenderScreen}
+          options={{ headerShown: false }}
+        />
+        {/* ⭐ New Hunt screen route */}
+        <Stack.Screen
+          name="Hunt"
+          component={HuntScreen}
+          options={{ headerShown: false }}
+        />
       </Stack.Navigator>
     </NavigationContainer>
   );
